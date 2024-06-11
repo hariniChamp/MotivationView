0691190cff411524f52da2deb79286a2ad91e7dc
Harini <harinichamp02@gmail.com>
2024-06-11 13:02:59 -0500

    Initial Commit

---
+++ MotivationView/MotivationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationViewApp.swift
+//  MotivationView
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MotivationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
